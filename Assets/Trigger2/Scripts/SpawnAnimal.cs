ec42cf3a57774293cb1e3af57c8d11c909e8790c
LeoYarey <156944764+LeoYarey@users.noreply.github.com>
2024-09-28 21:39:26 -0600

    aaa
    aaa

--- Assets/Trigger2/Scripts/SpawnAnimal.cs
+++ Assets/Trigger2/Scripts/SpawnAnimal.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class SpawnAnimal : MonoBehaviour
 {
-    public GameObject animalPrefab;  // El prefab del animal que será instanciado
-    public Transform spawnPoint;     // El punto donde el animal aparecerá
+    public GameObject animalPrefab;  
+    public Transform spawnPoint;     
 
-    private GameObject animalInstanciado; // Referencia al animal instanciado
+    private GameObject animalInstanciado; 
 
-    // Método que se llama cuando algo entra en el trigger
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && animalInstanciado == null)
         {
-            // Instanciar el animal en la posición y rotación del spawn point
+            
             animalInstanciado = Instantiate(animalPrefab, spawnPoint.position, spawnPoint.rotation); 
         }
     }
 
-    // Método que se llama cuando algo sale del trigger
+    
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player") && animalInstanciado != null)
         {
-            // Destruye el animal instanciado (despawn)
+            
             Destroy(animalInstanciado); 
-            animalInstanciado = null;   // Resetea la referencia a null
+            animalInstanciado = null;   
         }
     }
 }
